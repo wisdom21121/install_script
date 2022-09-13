c2b0f32f7b4bc0878ac5969d37e9112ab302542a
wisdom21121 <94742352+wisdom21121@users.noreply.github.com>
2022-09-13 19:40:59 +0100

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -41,9 +41,9 @@ echo "127.0.0.1    localhost" >> /etc/hosts
 echo "::1          localhost" >> /etc/hosts
 echo "127.0.1.1    Arch.localdomain    Arch" >> /etc/hosts
 
-pacman -S man-db man-pages inetutils netctl dhcpcd networkmanager network-manager-applet wpa_supplicant wireless_tools dialog grub efibootmgr dosfstools mtools firewalld xorg-server xorg-xinit alsa-utils pulseaudio pavucontrol bash-completion firefox vlc mpv neofetch htop reflector python-pip flameshot noto-fonts noto-fonts-emoji gwenview sweeper ffmpegthumbs gparted
+pacman -S man-db man-pages inetutils netctl dhcpcd networkmanager network-manager-applet wpa_supplicant wireless_tools dialog grub efibootmgr dosfstools mtools firewalld xorg-server xorg-xinit alsa-utils pulseaudio pavucontrol bash-completion firefox vlc mpv neofetch htop reflector python-pip flameshot noto-fonts noto-fonts-emoji gwenview sweeper ffmpegthumbs gparted okular
 
-pacman -S sddm plasma-meta dolphin dolphin-plugins konsole okular 
+pacman -S sddm plasma-meta dolphin dolphin-plugins konsole 
 
 # The dkms module is used if you want more than one kernel installed.
 #pacman -S nvidia nvidia-lts nvidia-dkms nvidia-utils nvidia-settings
