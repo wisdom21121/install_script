b39d19d63636fe489a948407e18b2e6ed81d3650
wisdom21121 <94742352+wisdom21121@users.noreply.github.com>
2023-09-26 10:30:10 +0100

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -42,9 +42,9 @@ echo "127.0.0.1    localhost" >> /etc/hosts
 echo "::1          localhost" >> /etc/hosts
 echo "127.0.1.1    Arch.localdomain    Arch" >> /etc/hosts
 
-pacman -S man-db man-pages inetutils netctl dhcpcd networkmanager network-manager-applet wpa_supplicant wireless_tools dialog grub efibootmgr dosfstools mtools firewalld xorg-server xorg-xinit alsa-utils wget pulseaudio pavucontrol bash-completion firefox mpv neofetch htop reflector noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji ffmpegthumbs ntfs-3g unrar
+pacman -S man-db man-pages inetutils netctl dhcpcd networkmanager network-manager-applet wpa_supplicant wireless_tools dialog grub efibootmgr dosfstools mtools firewalld xorg-server xorg-xinit alsa-utils wget pulseaudio pavucontrol bash-completion firefox reflector
 
-pacman -S sddm plasma-meta dolphin dolphin-plugins konsole okular sweeper gwenview ark
+pacman -S sddm plasma-meta dolphin dolphin-plugins konsole
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
